7f82019050820fe0d5dd866ba1ea42dd50cc3c86
Murad Eynizada <eynizadamurad@gmail.com>
2021-08-27 01:29:22 +0900

    bismillah v3

--- frontend/src/components/card.component.js
+++ frontend/src/components/card.component.js
@@ -8,25 +8,39 @@ export default class Card extends Component {
             title: this.props.title,
             type: this.props.type,
             datetime: this.props.datetime,
-            rewardeffort: this.props.rewardeffort
+            rewardeffort: this.props.rewardeffort,
+            location: this.props.location,
+            time: this.props.time, 
         }
     }
     render() {  
         return (
+        
             <div className = 'card_wrapper'>
+               <div className="card mb-3" style={{maxWidth: '540px'}}>
+  <div className="row g-0">
+    <div className="col-md-4">
                 <img src = {logo} width = "100px" height = "100px" />
+                </div>
+                <div className ='col-md-8'>
                 <div className = 'in_card_wrapper'>
-                    <h5 className = 'cardHeaders'>{this.state.title}</h5>
+                    <h5 style={{textAlign:'left'}}>{this.state.title}</h5>
                     <hr></hr>
-                    <h7 className = 'cardHeaders'>{this.state.type}</h7>
-                    <h7 className = 'cardHeaders'>{this.state.datetime}</h7>
-                    <h7 className = 'cardHeaders'>{this.state.rewardeffort}</h7>
+                    <h6 className = 'cardHeaders' style={{textAlign:'left'}}><strong>Type: </strong> {this.state.type}</h6>
+                    <h6 className = 'cardHeaders' style={{textAlign:'left'}}>Date: {this.state.datetime}</h6>
+                    <h6 className = 'cardHeaders' style={{textAlign:'left'}}>Time: {this.state.time}</h6>
+                    <h6 className = 'cardHeaders' style={{textAlign:'left'}}>Location: {this.state.location}</h6>
+                    <h6 className = 'cardHeaders' style={{textAlign:'left'}}>Reward: {this.state.rewardeffort} Won</h6>
                     <hr></hr>
                     <div className="progress" style = {{height: '10px'}}>
                         <div className="progress-bar" role="progressbar" style={{width: "25%"}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
+                    </div>
+                    </div>
+                    </div>
                 </div>
             </div>
+            
         );
     }
 }
