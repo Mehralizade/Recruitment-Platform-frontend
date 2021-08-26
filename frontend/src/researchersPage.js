c29a658320302c35f2b27bac491b9d4dc5418fd0
Murad Eynizada <eynizadamurad@gmail.com>
2021-08-27 01:23:57 +0900

    i am an atheist but bismillah

---
+++ frontend/src/researchersPage.js
@@ -0,0 +1,12 @@
+import React, { Component, useState, useEffect, } from "react";
+import Researcher_Profile from "./components/Researcher_Profile";
+import MyExperiments from "./myExperiments";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+export default function MyExperiments(props) {
+    return (
+        <BrowserRouter>
+            
+        </BrowserRouter>
+    );
+}
